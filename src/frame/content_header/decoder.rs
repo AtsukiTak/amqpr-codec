de39298057ed8fe899e87c8171df56dfb06f0256
AtsukiTak <takatomgoo@gmail.com>
2017-12-05 16:26:04 +0900

    Add assert to check ContentHeaderPayload

--- src/frame/content_header/decoder.rs
+++ src/frame/content_header/decoder.rs
@@ -6,10 +6,15 @@ use super::ContentHeaderPayload;
 
 pub fn decode_payload(payload: BytesMut) -> ContentHeaderPayload {
     let mut cursor = Cursor::new(payload);
+
     let class_id = cursor.get_u16::<BigEndian>();
+    assert!(class_id != 0);
+
     let weight = cursor.get_u16::<BigEndian>(); // must be zero
     assert_eq!(weight, 0);
+
     let body_size = cursor.get_u64::<BigEndian>();
+
     let property_flags = cursor.get_u16::<BigEndian>();
 
     let payload = ContentHeaderPayload {
